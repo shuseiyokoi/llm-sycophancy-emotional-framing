fd90b0f4ee5b45556556ec28b77e87a85d678204
shuseiyokoi <myoubann613@gmail.com>
2026-06-04 14:21:58 -0700

    able to handle multiple types of prompts and update results

--- src/config.py
+++ src/config.py
@@ -1,8 +1,17 @@
 PATH_TO_DATA = "../data/"
 PATH_TO_RESULTS = "../results/"
-NUM_ITERATIONS = 1
+NUM_ITERATIONS = 10
 
 GPT_MODEL = "gpt-3.5-turbo"
 CLAUDE_MODEL = "claude-haiku-4-5-20251001"
 GEMINI_MODEL = "gemini-2.5-flash-lite"
 # QWEN_MODEL = "qwen-3.5"
+
+# prompt_jobs_config.py
+
+PROMPT_TYPES = [
+    "control_prompt",
+    "emotional_prompt",
+    "identity_actual_prompt",
+    "emotional_identity_prompt",
+]
