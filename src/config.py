b3e5979955ffe637b71a2dcec8be55b4418109cc
Shusei Yokoi <your-email@example.com>
2026-07-06 22:46:28 -0700

    fix data and rerun missed iter

--- src/config.py
+++ src/config.py
@@ -2,12 +2,15 @@ PATH_TO_DATA = "../data/"
 PATH_TO_RESULTS = "../results/"
 NUM_ITERATIONS = 300
 
-GPT_MODELS = ["gpt-3.5-turbo", "gpt-4o-mini"]
+# GPT_MODELS = ["gpt-3.5-turbo", "gpt-4o-mini"]
 CLAUDE_MODELS = [
-    "claude-haiku-4-5-20251001",
-    "claude-sonnet-4-5-20250929",
+    # "claude-haiku-4-5-20251001",
+    # "claude-sonnet-4-5-20250929",
 ]  # claude-sonnet-4-20250514 gives non json responses
-GEMINI_MODELS = ["gemini-2.5-flash-lite", "gemini-2.5-pro"]
+GEMINI_MODELS = [
+    # "gemini-2.5-flash-lite",
+    "gemini-2.5-pro"
+]
 
 # QWEN_MODELS = [
 #     "qwen3:8b",
@@ -17,10 +20,10 @@ GEMINI_MODELS = ["gemini-2.5-flash-lite", "gemini-2.5-pro"]
 # prompt_jobs_config.py
 
 PROMPT_TYPES = [
-    "control_prompt",
-    "emotional_identity_prompt",
-    "emotional_prompt",
-    "emotional_extreme_prompt",
+    # "control_prompt",
+    # "emotional_identity_prompt",
+    # "emotional_prompt",
+    # "emotional_extreme_prompt",
     "emotional_suicidal_prompt",
     "identity_prompt",
     "identity_hypothetical_prompt",
